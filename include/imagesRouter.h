79adc3ae352bbc999c399f56977c9d3fe260b645
Yue_Ting <127368289+soda1210@users.noreply.github.com>
2024-07-26 03:59:24 +0800

    add Flip images

---
+++ include/imagesRouter.h
@@ -0,0 +1,45 @@
+#ifndef _IMAGE_ROUTER_H
+#define _IMAGE_ROUTER_H
+
+#include <Arduino.h>
+
+// 白畫面
+#include "images/white.h"
+// CHC 圖有部分異常
+#include "images/CHC.h"
+// 尋車 車錶電池 右上角
+#include "images/watch_battery.h"
+// 尋車 車錶電力不足警告
+#include "images/watch_battery_warning.h"
+// 尋車 車鎖 上鎖
+#include "images/lock.h"
+// 尋車 車鎖 解鎖
+#include "images/unlock.h"
+// 尋車 蜂鳴器 大自行車
+#include "images/big_bike.h"
+// 尋車 蜂鳴器 左訊號
+#include "images/left_single.h"
+// 尋車 蜂鳴器 右訊號
+#include "images/right_single.h"
+// 尋車 自行車路徑
+#include "images/distance.h"
+// 尋車 距離單位 M
+#include "images/m.h"
+// 尋車 無訊號
+#include "images/no_signal.h"
+// 車錶 腳踏車圖標 左上角
+#include "images/small_bike.h"
+// 車錶 自行車電量
+#include "images/bike_battery.h"
+// 車錶 自行車電力警告
+#include "images/bike_battery_warning.h"
+// 車錶 自動模式
+#include "images/assist_mode_auto.h"
+// 車錶 正常模式
+#include "images/assist_mode_normal.h"
+// 車錶 速度單位
+#include "images/km_h.h"
+// 車錶 速度圖示
+#include "images/speed_icon.h"
+
+#endif
