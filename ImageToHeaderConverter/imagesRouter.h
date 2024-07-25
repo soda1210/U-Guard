afaddba791719780278f4595c5af3acb260656f5
Yue_Ting <127368289+soda1210@users.noreply.github.com>
2024-07-26 03:51:36 +0800

    Add battery Images

--- ImageToHeaderConverter/imagesRouter.h
+++ ImageToHeaderConverter/imagesRouter.h
@@ -7,9 +7,9 @@
 #include "images/white.h"
 // CHC 圖有部分異常
 #include "images/CHC.h"
-// 車錶電池 右上角
-// #include "images/watch_battery.h"
-// 車錶電力不足警告
+// 尋車 車錶電池 右上角
+#include "images/watch_battery.h"
+// 尋車 車錶電力不足警告
 #include "images/watch_battery_warning.h"
 // 尋車 車鎖 上鎖
 #include "images/lock.h"
@@ -30,7 +30,7 @@
 // 車錶 腳踏車圖標 左上角
 #include "images/small_bike.h"
 // 車錶 自行車電量
-// #include "images/bike_battery.h"
+#include "images/bike_battery.h"
 // 車錶 自行車電力警告
 #include "images/bike_battery_warning.h"
 // 車錶 自動模式
@@ -40,6 +40,6 @@
 // 車錶 速度單位
 #include "images/km_h.h"
 // 車錶 速度圖示
-// #include "images/speed_icon.h"
+#include "images/speed_icon.h"
 
 #endif
